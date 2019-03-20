204864e83ec5f247ee628916781536ffb3b1de6b
macjabeth <macbethjonathan@gmail.com>
2019-03-20 21:21:35 +0000

    create the posts router

---
+++ api/routes/posts.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+const postDB = require('../../data/helpers/postDb');
+
+// C - POST
+router.post('/', async (req, res) => {
+  let { body: post } = req;
+
+  if (!post.text) {
+    return res.status(400).json({
+      error: 'Please provide text for the post.'
+    });
+  }
+
+  try {
+    post = await postDB.insert(post);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({
+      error: `There was an error while saving the post to the database; ${error}`
+    });
+  }
+});
+
+// R - GET
+router.get('/', async (req, res) => {
+  try {
+    const posts = await postDB.get();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({
+      error: `The posts information could not be retrieved; ${error}`
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { params: { id } } = req;
+  try {
+    const post = await postDB.getById(id);
+    Boolean(post)
+      ? res.status(200).json(post)
+      : res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+  } catch (error) {
+    res.status(500).json({
+      message: `The post information could not be retrieved; ${error}`
+    });
+  }
+});
+
+// U - PUT
+router.put('/:id', async (req, res) => {
+  const {
+    body: post,
+    params: { id }
+  } = req;
+
+  if (!post.text) {
+    return res.status(400).json({
+      error: 'Please provide text for the post.'
+    });
+  }
+
+  try {
+    const count = await postDB.update(id, post);
+    Boolean(count)
+      ? res.status(200).json(await postDB.getById(id))
+      : res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+  } catch (error) {
+    res.status(500).json({
+      error: `The post information could not be modified; ${error}`
+    });
+  }
+});
+
+// D - DELETE
+router.delete('/:id', async (req, res) => {
+  const { params: { id } } = req;
+
+  try {
+    const count = await postDB.remove(id);
+    Boolean(count)
+      ? res.status(200).json({ message: 'The post has been deleted.', id })
+      : res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+  } catch (error) {
+    res.status(500).json({
+      error: `The post could not be removed; ${error}`
+    });
+  }
+});
+
+module.exports = router;
